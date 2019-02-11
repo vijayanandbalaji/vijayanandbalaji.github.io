cdbc819d8c83faad9eb00e3869fb61a545f023f7
vijayanandbalaji <40574837+vijayanandbalaji@users.noreply.github.com>
2019-02-11 10:10:42 +0530

    Delete app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-  var progressBars = document.querySelectorAll('div[role="progressbar"]');
-  var width;
-  progressBars.forEach(function(progressBarObject){
-    width = progressBarObject.getAttribute('aria-valuenow');
-    progressBarObject.style.width = width + '%';
-  });
-}
